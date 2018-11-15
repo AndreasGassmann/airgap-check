dfcfcd54ab493d8ca7b394a2f714cf72646ee636
Andreas Gassmann <andreas@andreasgassmann.ch>
2018-11-15 21:46:45 +0100

    feat(prettier): add and run prettier

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Platform } from '@ionic/angular';
-import { NavigationStart, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core'
+import { Platform } from '@ionic/angular'
+import { NavigationStart, Router } from '@angular/router'
+import { Subscription } from 'rxjs'
 
-declare var QRScanner: any;
+declare var QRScanner: any
 
 @Component({
   selector: 'app-about',
@@ -11,7 +11,7 @@ declare var QRScanner: any;
   styleUrls: ['about.page.scss']
 })
 export class AboutPage implements OnInit {
-  private routeSub?: Subscription; // subscription to route observer
+  private routeSub?: Subscription // subscription to route observer
 
   constructor(private platform: Platform, private router: Router) {}
 
@@ -19,37 +19,37 @@ export class AboutPage implements OnInit {
     if (this.platform.is('cordova')) {
       this.routeSub = this.router.events.subscribe((event: any) => {
         if (event instanceof NavigationStart) {
-          this.stop();
+          this.stop()
         }
-      });
+      })
 
-      QRScanner.scan(this.displayContents.bind(this));
-      QRScanner.show();
+      QRScanner.scan(this.displayContents.bind(this))
+      QRScanner.show()
     } else {
-      console.log('Not cordova');
+      console.log('Not cordova')
     }
   }
 
   private async displayContents(err: any, text: string) {
     if (err) {
       // an error occurred, or the scan was canceled (error code `6`)
-      console.error('qr callback', err);
+      console.error('qr callback', err)
     } else {
       // The scan completed, display the contents of the QR code
-      alert(text);
+      alert(text)
     }
   }
 
   stop() {
     // TODO: Call this when navigating away
     QRScanner.destroy((status: any) => {
-      console.log(status);
-    });
+      console.log(status)
+    })
   }
 
   public ngOnDestroy() {
     if (this.routeSub) {
-      this.routeSub.unsubscribe();
+      this.routeSub.unsubscribe()
     }
   }
 }
